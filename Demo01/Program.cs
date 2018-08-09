a72da3f1cfe105f0014a7e3edd9c793a78fa44be
Cookie_CN <494977830@qq.com>
2018-08-09 19:10:25 +0800

    新仓库

---
+++ Demo01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Student> list = new List<Student>();
+            //var result = list.Find(stu => stu.Name == "");
+            //Console.WriteLine(result==null);
+
+            double b = 82.0 / 98598;
+            Console.WriteLine(b);
+            var r = Math.Round(Convert.ToDecimal(b), 2);
+            Console.WriteLine(r);
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
